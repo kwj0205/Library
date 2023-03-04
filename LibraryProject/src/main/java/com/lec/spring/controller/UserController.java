0a9fc2fc3f520eac730eac49edfc506583b6f6bd
dnwo0025 <rladnwo0205@naver.com>
2023-03-04 22:20:09 +0900

    회원정보 수정 페이지 최종본
    회원정보 수정 페이지

--- LibraryProject/src/main/java/com/lec/spring/controller/UserController.java
+++ LibraryProject/src/main/java/com/lec/spring/controller/UserController.java
@@ -3,13 +3,8 @@ package com.lec.spring.controller;
 import com.lec.spring.domain.User;
 import com.lec.spring.domain.UserValidator;
 import com.lec.spring.service.UserService;
-import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -22,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
-import java.util.logging.Logger;
 
 @Controller
 @RequestMapping("/user")
@@ -110,28 +104,28 @@ public class UserController {
             , Model model
             , RedirectAttributes redirectAttrs
     ){
-        // 이미 등록된 중복된 아이디(username) 이 들어오면
-//        if(!result.hasFieldErrors("username") && userService.isExist(user.getUsername())){
-//            result.rejectValue("username", "이미 존재하는 아이디입니다");
-//        }
-//
-//        // 검증 에러가 있었다면 redirect 한다
-//        if(result.hasErrors()){
-//            redirectAttrs.addFlashAttribute("username", user.getUsername());
-//            redirectAttrs.addFlashAttribute("name", user.getName());
-//            redirectAttrs.addFlashAttribute("phonenumber", user.getPhonenumber());
-//            redirectAttrs.addFlashAttribute("email", user.getEmail());
-//
-//            List<FieldError> errList = result.getFieldErrors();
-//            for(FieldError err : errList){
-//                redirectAttrs.addFlashAttribute("error", err.getCode());    // 가장 처음에 발견된 에러를 담아 보냄
-//                break;
-//            }
-//
-//            return "redirect:/user/userUpdate?id=" + user.getId();
-//        }
+        // 이미 등록된 중복된 전화번호나 이메일이 들어오면
+        if(!result.hasFieldErrors("phonenumber") && userService.isPhonenumber(user.getPhonenumber())){
+            result.rejectValue("phonenumber", "이미 존재하는 전화번호입니다");
+        } else if (!result.hasFieldErrors("email") && userService.isEmail(user.getEmail())) {
+            result.rejectValue("email", "이미 존재하는 이메일입니다");
+        }
 
+        // 검증 에러가 있었다면 redirect 한다
+        if(result.hasErrors()){
+            redirectAttrs.addFlashAttribute("username", user.getUsername());
+            redirectAttrs.addFlashAttribute("name", user.getName());
+            redirectAttrs.addFlashAttribute("phonenumber", user.getPhonenumber());
+            redirectAttrs.addFlashAttribute("email", user.getEmail());
 
+            List<FieldError> errList = result.getFieldErrors();
+            for(FieldError err : errList){
+                redirectAttrs.addFlashAttribute("error", err.getCode());    // 가장 처음에 발견된 에러를 담아 보냄
+                break;
+            }
+
+            return "redirect:/user/userUpdate?id=" + user.getId();
+        }
 
         model.addAttribute("result", userService.userUpdate(user));
         model.addAttribute("dto", user);
